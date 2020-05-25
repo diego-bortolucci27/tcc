b0245ecbfac59e30e3ff177dae409264d0bebe37
Diego Bortolucci <bortolucci.diego@gmail.com>
2020-05-25 19:50:36 -0300

    Inicio do projeto
    Coloquei o bootstrap, os links do css e do JavaScript e também começei a fazer o navbar

--- index.php
+++ index.php
@@ -2,10 +2,42 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="web/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="web/css/estilo.css">
+    <link rel="stylesheet" href="web/css/grid.css">
+    <link rel="stylesheet" href="web/css/reset.css">
+
+    <title>Página Inicial</title>
 </head>
 <body>
-    
+    <div class="principal container-fluid">
+        <header class="header">
+            <nav class="navbar" >
+                <table>
+                    <tr>
+                        <a style="color: white;" href="index.php"> Home </a>
+                        <a style="color: white;" href="web/php/contato.php"> Contato </a>
+                        <a style="color: white;" href="web/php/sobre.php"> Sobre </a>
+                        <a style="color: white;" href="web/php/ajuda.php"> Ajuda </a>
+                    </tr>
+                </table>
+            </nav>
+        </header>
+
+
+        <footer></footer>
+    </div>
+
+
+
+
+<script src="web/javaScript/script.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 </body>
 </html>
