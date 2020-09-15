4c583c4e0c4b43b64f458659e5aaf9d428e84ad7
ViniciusGabrielFreitas <viniciusgabriel18.05@gmail.com>
2020-09-15 12:32:48 -0300

    Inicio do layout da pagina ajuda

--- ajuda.php
+++ ajuda.php
@@ -1,3 +1,122 @@
 <?php
 
 ?>
+
+
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="web/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="web/css/estilo.css">
+    <link rel="stylesheet" href="web/css/grid.css">
+    <link rel="stylesheet" href="web/css/reset.css">
+     <title>Ajuda</title>
+ </head>
+ <body>
+     
+  <!-- NAV BAR-->
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">logo etec</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+      </li>
+    </ul>
+  </div>
+</nav>
+
+ <!-- FIM NAV BAR -->
+
+  <!-- INFO 1 -->
+
+  <div class="jumbotron jumbotron-fluid" style='background-color: #DC300B'>
+  <div class="container.fluid">
+    <h1 class="display-4" class="ajuda_sobre" style='text-align:center; color:white;'>SOBRE</h1>
+    <p class="lead" style='color:white; margin:25px;'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere voluptas accusamus sint maiores deleniti, doloremque similique, esse unde suscipit ducimus animi vero magni cumque obcaecati velit aliquam iusto eveniet illo? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, incidunt omnis optio sequi harum vel, inventore possimus cupiditate, quam placeat non tenetur eaque natus fuga quod molestiae officia consequuntur dolor? Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptatibus, distinctio reprehenderit nulla minus quisquam ea! Est laborum perspiciatis corrupti quidem numquam pariatur laudantium repudiandae animi aperiam vitae, ducimus quis! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, dolores. Quod eligendi ex nobis ducimus voluptate, cupiditate natus expedita fugiat similique! Quasi, earum! Amet saepe vel animi pariatur atque! Provident. Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis doloremque minima iste? Molestiae voluptatem explicabo possimus eos sequi alias est sed voluptatum dignissimos! Ex, iure sunt. In corrupti vero explicabo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum omnis consequatur natus dolore culpa sit distinctio eos enim. Hic facilis cumque voluptas odit deleniti at beatae accusamus voluptatem earum libero. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam, perferendis numquam? Iure quia libero itaque a! Amet doloremque culpa, eveniet ad labore est nostrum commodi cumque odio reiciendis voluptates quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente minus, facilis porro laborum labore laboriosam? Autem minima ab hic. Eos voluptatum quas delectus recusandae, aspernatur repellat error dolorum eveniet amet?</p>
+  </div>
+</div>
+
+   <!-- FIM INFO 1-->
+
+    <!--contato-->
+    <div class="jumbotron jumbotron-fluid" style='background-color: #DC300B'>
+        <div class="container.fluid">
+        <h1 class="display-4" class="ajuda_sobre" style='text-align:left; color:white; margin-left:130px; font-size: 30px;'>CONTATO</h1>
+        </div>
+
+            <!-- tabela --> 
+    <section class="tabela" style='width: 450px;height:350px; margin-left: 20px;'>  
+    <table class="table" style='color:white'>
+  <tbody>
+    <tr>
+      <th scope="row">Diego Lopes Bortolucci</th>
+      <td>email@merkadinho</td>
+    </tr>
+    <tr>
+      <th scope="row">Filipi Henrique Pérez</th>
+      <td>email@filipi</td>
+    </tr>
+    <tr>
+      <th scope="row">Marcus Andreoli</th>
+      <td>email@marcus</td>
+    </tr>
+    <tr>
+      <th scope="row">Viniciús Gabriel Freitas</th>
+      <td>viniciusgabriel18.05@gmail.com</td>
+    </tr>
+    <tr>
+      <th scope="row">Vitor Ádrian Vieira</th>
+      <td>email@vitão</td>
+    </tr>
+  </tbody>
+</table>
+    <!-- fim tabela --> 
+    </section>
+ 
+            <!-- fim tabela --> 
+
+        <section class='ajuda_mensagem' style='width: 500px; float:none; margin-top: -350px; margin-left: 700px;color:white;'>
+        <form>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Email</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Deixe sua dúvida aqui</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+</form>
+
+        </section>
+
+    </div>
+
+
+    <!--fim contato-->
+
+
+
+
+ </body>
+ </html>
