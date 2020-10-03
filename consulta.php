fa2d8bebdc4900225502414531efd3e7693341b0
diego-bortolucci27 <bortolucci.diego@gmail.com>
2020-10-02 21:45:31 -0300

    Pequenas alterações

--- consulta.php
+++ consulta.php
@@ -16,6 +16,7 @@
     <title>Consulta</title>
 </head>
 <body>
+<div class="container-fluid">
     <div>
         <center>
             <h1 class="titulo"><b>Consulta de Dados - Alunos<b></h1>
@@ -38,10 +39,10 @@
             <div class="dropdown-menu">
                 <a class="dropdown-item" href="consultaAdm.php">Ver ETIM ADM</a>
                 <a class="dropdown-item" href="consultaInfo.php">Ver ETIM INFO</a>
+            </div>
         </div>
     </div>
-</div>
-    </div>
+
     <div class="table-responsive">
         <table class="table">
             <thead class="bg-danger">
@@ -82,9 +83,10 @@
             ?>
         </table>
     </div>
-    <script src="web/javaScript/script.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</div>
+<script src="web/javaScript/script.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
