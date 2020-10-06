edca2d70e446b62ab567cdf31a9502f60dd307d3
Diego Bortolucci <bortolucci.diego@gmail.com>
2020-10-05 23:55:48 -0300

    alterações no index

--- cadastro.php
+++ cadastro.php
@@ -8,66 +8,67 @@
 <html lang="pt-br">
 <head>
     <title>Cadastro de Alunos</title>
+    <link rel="stylesheet" href="css/cadastro.css">
 </head>
-<body>
+<body class="body">
     <div class="principal container-fluid">
-        <h1>Cadastro dos Alunos</h1>
-        <br><br>
+        <h1 style="color: black; font-size: 16pt; margin: 15px;"><b>Cadastro dos Alunos</b></h1>
+        <br>
     <form action="inserir.php" method="POST">
         <div class="form-group col-md-6">
-            <label for="rm">RM</label>
+            <label style="color: black;" for="rm">RM</label>
             <input type="number" class="form-control" id="rm" name="rm" placeholder="EX: 17170">
         </div>
         <div class="form-group col-md-6">
-          <label for="nome">Nome Completo</label>
+          <label style="color: black;" for="nome">Nome Completo</label>
           <input type="text" class="form-control" id="nome" name="nome" aria-describedby="emailHelp" placeholder="EX: Luís Eduardo Santos">
         </div>
         <div class="form-group col-md-6">
-          <label for="serie">Série</label>
+          <label style="color: black;" for="serie">Série</label>
             <select class="custom-select" id="serie" name="serie">
-              <option selected hidden>Série</option>
-              <option value="1º ANO">1º ANO</option>
-              <option value="2º ANO">2º ANO</option>
-              <option value="3º ANO">3º ANO</option>
+              <option style="color: black;" selected hidden>Série</option>
+              <option style="color: black;" value="1º ANO">1º ANO</option>
+              <option style="color: black;" value="2º ANO">2º ANO</option>
+              <option style="color: black;" value="3º ANO">3º ANO</option>
             </select>
 
         </div>
         <div class="form-group col-md-6">
-          <label for="curso">Curso</label>
+          <label style="color: black;" for="curso">Curso</label>
           <div class="form-check">
             <input class="form-check-input" type="radio" name="curso" id="curso" value="ETIM INFOWEB">
-              <label class="form-check-label" for="info">
+              <label class="form-check-label" for="info" style="color: black;">
                 ETIM INFOWEB
               </label>
           </div>
           <div class="form-check">
             <input class="form-check-input" type="radio" name="curso" id="curso" value="ETIM ADM">
-              <label class="form-check-label" for="adm">
+              <label class="form-check-label" for="adm" style="color: black;">
                 ETIM ADM
               </label>
           </div>
         </div>
         <div class="form-group col-md-6">
-            <label for="telefone">Telefone</label>
+            <label style="color: black;" for="telefone">Telefone</label>
             <input type="number" class="form-control" id="telefone" name="telefone" placeholder="Ex: (19)99999-9999">
         </div>
         <div class="form-group col-md-6">
-          <label for="email">Email</label>
+          <label style="color: black;" for="email">Email</label>
           <input type="text" class="form-control" id="email" name="email" placeholder="Ex: luis_santos@etec.sp.gov.br">
         </div>
         <div class="form-group col-md-6">
-          <label for="responsavel">Nome do Responsável</label>
+          <label style="color: black;" for="responsavel">Nome do Responsável</label>
           <input type="text" class="form-control" id="responsavel" name="responsavel" aria-describedby="emailHelp" placeholder="Ex: Antônio Lopes Cardoso">
         </div>
         <div class="form-group col-md-6">
-            <label for="telefoneResponsavel">Telefone Responsável</label>
+            <label style="color: black;" for="telefoneResponsavel">Telefone Responsável</label>
             <input type="number" class="form-control" id="telResponsavel" name="telResponsavel" placeholder="Ex: (19)99999-9999">
         </div>
         <div class="form-group col-md-6">
-          <label for="emailResponsavel">Email Responsável</label>
+          <label style="color: black;" for="emailResponsavel">Email Responsável</label>
           <input type="text" class="form-control" id="emailResponsavel" name="emailResponsavel" placeholder="Ex: antonio_cardoso@gmail.com">
         </div>
-        <input type="submit" value="Cadastrar" class="btn btn-primary">
+        <input type="submit" value="Cadastrar" class="btn btn-outline-primary">
     </form>
     </div>
 
--- index.php
+++ index.php
@@ -71,6 +71,20 @@
         </center>
     </section>
     </center>
+    <br>
+    <!--
+    <div class="card text-center">
+        <div class="card-header">
+          Cadastrar
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Cadastro de Alunos</h5>
+          <p class="card-text">Faça o cadastro dos alunos</p>
+          <a href="#" class="btn btn-outline-primary">Ir para Cadastro</a>
+        </div>
+    </div>
+    -->
+
 </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
